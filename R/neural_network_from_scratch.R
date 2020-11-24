358e8230926ff34d5b66548611d65b6c6ff6e650
Adithi R. Upadhya <63432077+adithirgis@users.noreply.github.com>
2020-11-25 00:23:06 +0530

    tidy up

--- R/neural_network_from_scratch.R
+++ R/neural_network_from_scratch.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+
 set.seed(009)
 planar_dataset <- function(){
   set.seed(1)
@@ -9,11 +10,11 @@ planar_dataset <- function(){
   Y <- matrix(0, nrow = m, ncol = 1)
   a <- 4
   
-  for(j in 0:1){
+  for(j in 0:1) {
     ix <- seq((N * j) + 1, N * (j + 1))
     t <- seq(j * 3.12, (j + 1) * 3.12, length.out = N) + 
       rnorm(N, sd = 0.2)
-    r <- a*sin(4*t) + rnorm(N, sd = 0.2)
+    r <- a * sin(4*t) + rnorm(N, sd = 0.2)
     X[ix,1] <- r * sin(t)
     X[ix,2] <- r * cos(t)
     Y[ix,] <- j
@@ -44,13 +45,13 @@ y_test <- test$Y
 dim(y_test) <- c(length(y_test), 1)
 
 
-X_train <- as.matrix(X_train, byrow=TRUE)
+X_train <- as.matrix(X_train, byrow = TRUE)
 X_train <- t(X_train)
-y_train <- as.matrix(y_train, byrow=TRUE)
+y_train <- as.matrix(y_train, byrow = TRUE)
 y_train <- t(y_train)
-X_test <- as.matrix(X_test, byrow=TRUE)
+X_test <- as.matrix(X_test, byrow = TRUE)
 X_test <- t(X_test)
-y_test <- as.matrix(y_test, byrow=TRUE)
+y_test <- as.matrix(y_test, byrow = TRUE)
 y_test <- t(y_test)
 getLayerSize <- function(X, y, hidden_neurons, train = TRUE) {
   n_x <- dim(X)[1]
